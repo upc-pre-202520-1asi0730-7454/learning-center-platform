8159ae8695665e66cab1b6a64b4aeb9372b09cbd
Angel Velasquez <pcisavel@upc.edu.pe>
2025-11-13 15:56:04 -0500

    feat(iam): add bearer token security definition and requirement for api documentation.

--- ACME.LearningCenterPlatform.API/Shared/Infrastructure/Documentation/OpenApi/Configuration/Extensions/WebApplicationBuilderExtensions.cs
+++ ACME.LearningCenterPlatform.API/Shared/Infrastructure/Documentation/OpenApi/Configuration/Extensions/WebApplicationBuilderExtensions.cs
@@ -29,6 +29,29 @@ public static class WebApplicationBuilderExtensions
                         Url = new Uri("https://www.apache.org/licenses/LICENSE-2.0.html")
                     }
                 });
+            options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+            {
+                In = ParameterLocation.Header,
+                Description = "Please enter token",
+                Name = "Authorization",
+                Type = SecuritySchemeType.Http,
+                BearerFormat = "JWT",
+                Scheme = "bearer"
+            });
+            options.AddSecurityRequirement(new OpenApiSecurityRequirement
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Id = "Bearer",
+                            Type = ReferenceType.SecurityScheme
+                        }
+                    },
+                    Array.Empty<string>()
+                }
+            });
             options.EnableAnnotations();
         });
     }
